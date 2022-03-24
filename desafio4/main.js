2893a1f89492cb070f13258d85a4eec87bdb24e2
Jennifer <jennicer.c17@gmail.com>
2022-03-24 04:04:45 -0300

    se agrego un formulario en html

--- desafio4/main.js
+++ desafio4/main.js
@@ -118,4 +118,23 @@ class Vehiculo {
 const moto = new Vehiculo({ruedas: 2, carroceria: "mediana", marcaDestino: "BW"});
 
 
-moto.presentarVehiculo();
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
